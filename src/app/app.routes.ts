eefcd57ea566cefd7f4cee7d9ea4b8f8f9be17e4
Henry Teran <teranhenryatgmail.com>
2025-03-21 22:28:35 +0100

    ajout des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { OrderPage } from './pages/order-page/order-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'order',
+        component: OrderPage,
+    },
+    {
+        path: 'order',
+        component: OrderPage,
+    },
+    {
+        path: 'order',
+        component: OrderPage,
+    },
+    {
+        path: '',
+        component: OrderPage,
+    },
+    {
+        path: '**',
+        component: OrderPage,
+    },
+
+];
