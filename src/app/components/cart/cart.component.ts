e3f5731f501bd7f8414bb574dbc2265fd4d46a0b
Henry Teran <teranhenryatgmail.com>
2025-04-02 03:52:21 +0200

    ajout fonctionnalité de sauvegarde dans firebase pour les commandes

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../interfaces';
 import { NotificationService } from '../../services/notifications/notification.service';
+import { FirebeseApiService } from '../../services/firebese-api.service';
 
 @Component({
   selector: 'app-cart',
@@ -11,20 +12,35 @@ import { NotificationService } from '../../services/notifications/notification.s
 export class CartComponent {
   @Input() selectedRecipe: Recipe[] = [];
   @Output() removeSelectFromCart = new EventEmitter<Recipe>();
-
+  @Output() saveOrder = new EventEmitter<Recipe[]>();
+  
   cartOpen = false; // Panier fermé par défaut
-
+  
   toggleCart() {
     this.cartOpen = !this.cartOpen;
   }
-
-  constructor(private notificationService: NotificationService) {}
-
+  
+  test = this.selectedRecipe; 
+  
+  constructor(
+    private notificationService: NotificationService,
+    private readonly _apiFirebase: FirebeseApiService) {
+      console.log('test' , this.test); 
+    }
+    
+    
   removeFromCart(recipe: Recipe) {
     this.removeSelectFromCart.emit(recipe);
     this.notificationService.addMessage(`${recipe.title} retiré du panier !`);
     console.log(this.notificationService.messages);
    }
+
+   saveOrderFromCart(recipe:Recipe[]) {
+    this.saveOrder.emit(recipe);
+    console.log("bouton licl")
+
+   }
+
 
 
 
--- src/app/services/firebese-api.service.ts
+++ src/app/services/firebese-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { addDoc, collection, doc, Firestore, writeBatch } from '@angular/fire/firestore';
+import { Recipe, SimplifiedRecipe } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebeseApiService {
 
-  constructor() { }
+  constructor(private _firebase: Firestore) { } 
+
+
+  saveOrder = async (recipes: SimplifiedRecipe[]) => {
+    const ref = collection(this._firebase, 'orders');
+
+    // Enregistrer toute la commande dans un seul document
+    return await addDoc(ref, {
+      date: new Date(),
+      done: false,
+      paid: false,
+      orders: recipes
+    });
+  };
+  
+
+
+
 }
