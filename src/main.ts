a6aa5209b5deb87ca4de0ad05b4f405a480215d8
Henry Teran <teranhenryatgmail.com>
2025-04-02 10:59:01 +0200

    Ajout du home page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { OrderPage } from './app/pages/order-page/order-page';
+import { HomePageComponent } from './app/pages/home-page/home-page.component';
 
-bootstrapApplication(OrderPage, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(OrderPage, appConfig)
+//   .catch((err) => console.error(err));
+
+  
+bootstrapApplication(HomePageComponent, appConfig)
+.catch((err) => console.error(err));
