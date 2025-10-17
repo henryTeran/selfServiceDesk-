dfa0294bd0bcda8961410642423dffa381886afc
Full Stack developer <65180848+henryTeran@users.noreply.github.com>
2025-10-17 14:10:04 +0200

    Updated price.pipe.ts

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -8,7 +8,9 @@ export class PricePipe implements PipeTransform {
   transform(value: number | null | undefined, currency: string = 'CHF'): string {
     if (value === null || value === undefined || isNaN(value)) return '-';
 
-    const formatted = value.toLocaleString('fr-CH', {
+    const priceInCurrency = value / 100;
+
+    const formatted = priceInCurrency.toLocaleString('fr-CH', {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
     });
